5422aaebed9204e02c1eee9e1820c2c47001e4ac
Eleusis12 <franciscomeireles-10@hotmail.com>
2025-04-08 21:15:09 +0200

    Initial Commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
-    },
-    "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+  name: "Code Stitch Web Designs",
+  email: "info@chaud-froid.lu",
+  phoneForTel: "+352 26 48 00 26",
+  phoneFormatted: "+352 26 48 00 26",
+  address: {
+    lineOne: " 35 rue J-F Kennedy, Hall C1",
+    lineTwo: "",
+    city: "Steinsel",
+    state: "",
+    zip: "L-7327",
+    country: "LU",
+    mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+  },
+  socials: {
+    facebook: "https://www.facebook.com/",
+    instagram: "https://www.instagram.com/",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  domain: "https://www.example.com",
 };
